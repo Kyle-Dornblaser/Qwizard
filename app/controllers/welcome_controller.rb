55e49446f16cee2cdeaf1517ab4910be888d3f54
Kyle-Dornblaser <kdornblaser@gmail.com>
2014-10-24 15:15:32 -0400

    Merged with Jordan and Aida

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,11 @@ class WelcomeController < ApplicationController
         getCurrentQuestionInstance
     end
     question_instance = QuestionInstance.last
+
+    if Time.now.to_f - question_instance.created_at.to_f > 20
+      getCurrentQuestionInstance
+    end
+
     # TODO Refactor question type code
     question = Question.find(question_instance.question_id)
     question_type = question.question_type
@@ -17,12 +22,23 @@ class WelcomeController < ApplicationController
         question: question,
         choices: choices
     }
-    if (question_type == 1)
-      render "multiple_choice"
-    elsif (question_type == 2)
-      render "multiple_select"
-    elsif (question_type == 3)
-      render "short_answer"
+
+    already_answered = false
+    if current_user
+      if UserResponse.find_by_question_instance_id_and_user_id(question_instance.id, current_user.id)
+        render "already_answered"
+        already_answered = true
+      end
+    end
+
+    unless already_answered
+      if (question_type == 1)
+        render "multiple_choice"
+      elsif (question_type == 2)
+        render "multiple_select"
+      elsif (question_type == 3)
+        render "short_answer"
+      end
     end
 
   end
