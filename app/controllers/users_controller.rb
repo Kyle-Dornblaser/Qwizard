a0a45181aebc09b51ba015be2d921b7333a0b6bc
keyl10 <kdornblaser@gmail.com>
2014-11-07 19:47:44 +0000

    Fixed problem with registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin, except: [:signup]
+  before_action :authenticate_admin, except: [:signup, :create]
 
   # GET /users
   # GET /users.json
